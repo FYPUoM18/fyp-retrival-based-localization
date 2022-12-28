89e4a22ebdc0c645b1adbf3dd9050ca19e841362
mashkarharis <mashkarharis@gmail.com>
2022-12-28 21:17:39 +0530

    Update Locations

--- project-implementation/config.py
+++ project-implementation/config.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-hdf5datadir = "/home/mashkarharis/MyFiles/Semester 7/FYP/code/nilocdata-subset/unib/"  # Directory contains hdf5 files
-processed_hdf5_outputdir = "output/processed_data.hdf5" # Processed HDF5 Output Directory
+# Preprocessing Configs
+hdf5datadir = "C:\\Users\\mashk\\MyFiles\\Semester 7\\FYP\\code\\nilocdata-subset\\unib"  # Directory contains hdf5 files
+processed_hdf5_outputdir = "output\\processed_data.hdf5" # Processed HDF5 Output Directory
 locrounding = 0 # No of decimals to round the locations
 timerounding = 0 # No of decimals to round the time
 
