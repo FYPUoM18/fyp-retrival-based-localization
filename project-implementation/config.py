d9c4ce17e6b21be5b21cdf3b6dbb9500b223dbf8
mashkarharis <mashkarharis@gmail.com>
2022-12-29 20:56:43 +0530

    Added Batch Generator

--- project-implementation/config.py
+++ project-implementation/config.py
@@ -7,3 +7,12 @@ processed_hdf5_outputdir = "output\\processed_data.hdf5" # Processed HDF5 Output
 locrounding = 0 # No of decimals to round the locations
 timerounding = 0 # No of decimals to round the time
 
+# Train/Test/Val Batch Generator Configs
+batchsize = 72
+no_of_total_batches = 100
+train_ratio = 0.80
+test_ratio = 0.15
+val_ratio = 0.05
+train_hdf5_outputdir = "output\\train_data.hdf5"
+test_hdf5_outputdir = "output\\test_data.hdf5"
+val_hdf5_outputdir = "output\\val_data.hdf5"
--- project-implementation/main.py
+++ project-implementation/main.py
@@ -1,14 +1,22 @@
 import config
 from preprocessing.preprocessor import PreProcessor
+from batch_generator.batchgenerator import BatchGenerator
 
 steps=[2]
 
 if __name__ == "__main__":
 
     if 1 in steps:
+        print("\n### STEP 1 ###")
+        print("PREPROCESSING")
+        print("##############")
         preprocessor = PreProcessor(config)
         preprocessor.start()
     
 
     if 2 in steps:
-        pass
+        print("\n### STEP 2 ###")
+        print("BATCH GENERATION")
+        print("##############")
+        batchgenerator = BatchGenerator(config)
+        batchgenerator.start()
