0a3df78facd96bbc5d1dc5f0bf0e066a647844d7
Sandalu Karunasena <sandalu.18@cse.mrt.ac.lk>
2023-03-16 23:05:10 +0530

    generate acce,gyro,game_rv

--- project_implementation/train_test_val/train_test_val.py
+++ project_implementation/train_test_val/train_test_val.py
@@ -1,4 +1,5 @@
-
+import numpy as np
+import h5py
 
 class tran_test_val:
 
@@ -7,32 +8,24 @@ class tran_test_val:
     
     def generate(self):
         import h5py
-import numpy as np
 
-# Open the HDF5 file
-with h5py.File('example.hdf5', 'r') as f:
-    # Get the time dataset
-    time = f['synced/time'][:]
+    with h5py.File('example.hdf5', 'r') as f:
+       
+        time = f['synced/time'][:]
 
-    # Set the duration to 20 seconds
-    duration = 20.0
+        duration = 20.0
 
-    # Calculate the number of samples in the duration
-    num_samples = int(duration * 100) # Assumes a 100Hz sampling rate
+        num_samples = int(duration * 100) # Assumes a 100Hz sampling rate
 
-    # Iterate over a fixed number of samples
-    for i in range(10):
-        # Select a random starting time index
-        start_idx = np.random.randint(0, len(time) - num_samples)
+        # Iterate over a fixed number of samples
+        for i in range(10):
+            
+            start_idx = np.random.randint(0, len(time) - num_samples)
 
-        # Get the gyro data for the current range
-        gyro = f['synced/gyro'][start_idx:start_idx+num_samples]
+            gyro = f['synced/gyro'][start_idx:start_idx+num_samples]
+            acce = f['synced/acce'][start_idx:start_idx+num_samples]
+            game_rv = f['synced/game_rv'][start_idx:start_idx+num_samples]
 
-        # Get the accelerometer data for the current range
-        acce = f['synced/acce'][start_idx:start_idx+num_samples]
+            file_name = f'output_{start_idx:05d}.txt'
 
-        # Construct the output file name
-        file_name = f'output_{start_idx:05d}.txt'
-
-        # Save the data to a text file
-        np.savetxt(file_name, np.c_[gyro, acce], delimiter=' ')
+            np.savetxt(file_name, np.c_[gyro, acce, game_rv], delimiter=' ')
