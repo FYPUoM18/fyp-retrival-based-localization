b01f8a77321e5e3de323a6b29467757bfda99a2f
mashkarharis <mashkarharis@gmail.com>
2023-04-27 15:35:24 +0530

    Updated Instructions

--- project_implementation/config.py
+++ project_implementation/config.py
@@ -1,8 +1,8 @@
 # Steps
-steps = [3,4]
+steps = [1,2,3,4,5,6,7,8]
 
 # Root Dir
-root_dir = "C:\\Users\\mashk\\MyFiles\\Semester 8\\FYP\\code"
+root_dir = "C:\\Users\\mashk\\MyFiles\\Semester 8\\FYP\\code\\project_implementation\\outputs\\set2"
 
 # Step1: Generate CSV From HDF5
 hdf5datadir = {
@@ -33,50 +33,45 @@ hdf5datadir = {
     }
 
 }
-csv_out_dir = f"{root_dir}\\project_implementation\\outputs\\1. csv_data"
+csv_out_dir = f"{root_dir}\\1. csv_data"
 preferred_files = {
     'loc': 'computed/aligned_pos',
     'gyro': "synced/gyro",
     'acce': "synced/acce",
-    'game_rv': "synced/game_rv",
-    'ronin': "computed/ronin"
+    'game_rv': "synced/game_rv"
 }
 
 # Step2: Generate Train/Test/Val From HDF5
 freq = 200  # Dpoints Per Sec
-no_of_sec_per_split = 45  # No fo seconds considered for a split
-train_test_val_meta_file = f"{root_dir}\\project_implementation\\outputs\\train_test_val_meta.csv"
+no_of_sec_per_split = 45  # Windows Size
+train_test_val_meta_file = f"{root_dir}\\train_test_val_meta.csv"
 
 # Step3: Get RoNIN Trajectory
-sample_rate=200
-ronin_checkpoint = f"{root_dir}\\project_implementation\\ronin_resnet\\checkpoint_gsn_latest.pt"
-processed_hdf5_out_dir = f"{root_dir}\\project_implementation\\outputs\\2. processed_data"
+sample_rate=freq
+ronin_checkpoint = f"{root_dir}\\ronin_resnet\\checkpoint_gsn_latest.pt"
+processed_hdf5_out_dir = f"{root_dir}\\2. processed_data"
 
 # Step4 : Draw Trajectory
-traj_drawing_out_dir = f"{root_dir}\\project_implementation\\outputs\\3. traj_visualized"
+traj_drawing_out_dir = f"{root_dir}\\3. traj_visualized"
 
 # Step5 : Make Time Invariant
-invariant_domain_output_dir = f"{root_dir}\\project_implementation\\outputs\\4. invariant"
+invariant_domain_output_dir = f"{root_dir}\\4. invariant"
 segment_length = 0.1
 
 # Step6 : Image DB Generate
-image_db_loc = f"{root_dir}\\project_implementation\\outputs\\5. imageDB"
-image_db_meta_file = f"{root_dir}\\project_implementation\\outputs\\image_db_meta_file.csv"
+image_db_loc = f"{root_dir}\\5. imageDB"
+image_db_meta_file = f"{root_dir}\\image_db_meta_file.csv"
 window_size = 120  # Same Length Curve For All Images : Length = 220 x 0.1 = 22
 step_size = 50
 
 # Step7 : Build KDTree
-image_db_loc_kdtree = f"{root_dir}\\project_implementation\\outputs\\5. imageDB\\db"
-kdtree_features_loc = f"{root_dir}\\project_implementation\\outputs\\kdtree_features.pickle"
-kdtree_tags_loc = f"{root_dir}\\project_implementation\\outputs\\kdtree_tags.pickle"
-
-# Eval
-# DTWThreshold_Expected_Real_LOC_Predicted__REAL_LOC_SEQS = 100
-# window_size = 120
+image_db_loc_kdtree = f"{root_dir}\\5. imageDB\\db"
+kdtree_features_loc = f"{root_dir}\\kdtree_features.pickle"
+kdtree_tags_loc = f"{root_dir}\\kdtree_tags.pickle"
 
 # Step8 : History Model
-to_eval_dir = "C:\\Users\\mashk\\MyFiles\\Semester 8\\FYP\\code\\project_implementation\\outputs\\4. invariant\\test"
-history_output_loc="C:\\Users\\mashk\\MyFiles\\Semester 8\\FYP\\code\\project_implementation\\outputs\\6. History"
+to_eval_dir = f"{root_dir}\\4. invariant\\test"
+history_output_loc=f"{root_dir}\\6. History"
 db_meta_csv = image_db_meta_file
 invariant_dir = invariant_domain_output_dir
 
--- project_implementation/main.py
+++ project_implementation/main.py
@@ -11,6 +11,8 @@ steps = config.steps
 
 if __name__ == "__main__":
 
+    print("============================================\nUpdate Following Configs\n------------------------\n1. Run\n\t- steps : steps to run\n\n2. Dataset to Train\n\t- root dir : root directory\n\t- hdf5datadir : db/train/test/val/mobile directory\n\n3. Generate Train Test Val Data\n\t- freq : capturing frequency of data [Hz][*]\n\t- no_of_sec_per_split : window size for random cropping for train/test/val [s][*]\n\n4. Generate RoNIN Trajectories\n\t- ronin_checkpoint : where to find pretrained RoNIN ResNET Model\n\n5. Make Time Invariant\n\t- segment_length : segment length for resampling after interpolation [m][*]\n\n6. ImageDB Generate\n\t- window_size : no of segments per curve caputured for single image [*]\n\t- step_size : no of segments to skip for next image [*]\n\n[m] : Measured in Meters\n[Hz] : Measured in Hz\n[s] : Measured in Seconds\n[*] : Tune/Change According to Dataset\n============================================\n\n")
+
     if 1 in steps:
         csv_generator = CSV_Generator(config)
         csv_generator.generate()
@@ -30,6 +32,7 @@ if __name__ == "__main__":
     if 5 in steps:
         domain_convertor = DomainConverter(config)
         domain_convertor.make_time_invariant()
+
     if 6 in steps:
         generate_imagedb = DBManager(config)
         generate_imagedb.generateImageDB()
@@ -41,4 +44,3 @@ if __name__ == "__main__":
     if 8 in steps:
         historyModel=HistoryModel(config)
         historyModel.process()
-
